2c4ca77a11e83fe0d745f7ad2b2b8c8a0cb2d8b5
AlloVince <allo.vince@gmail.com>
2018-08-04 21:56:10 +0800

    feat: init version

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-tslibrary [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> 
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-tslibrary using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-tslibrary
+```
+
+Then generate your new project:
+
+```bash
+yo tslibrary
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [AlloVince]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-tslibrary.svg
+[npm-url]: https://npmjs.org/package/generator-tslibrary
+[travis-image]: https://travis-ci.org/AlloVince/generator-tslibrary.svg?branch=master
+[travis-url]: https://travis-ci.org/AlloVince/generator-tslibrary
+[daviddm-image]: https://david-dm.org/AlloVince/generator-tslibrary.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/AlloVince/generator-tslibrary
---
+++ generators/app/index.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const mkdirp = require('mkdirp');
+const fg = require('fast-glob');
+
+module.exports = class extends Generator {
+  prompting() {
+    // Have Yeoman greet the user.
+    this.log(
+      yosay(`Welcome to the primo ${chalk.red('generator-typelibrary')} generator!`)
+    );
+
+    const prompts = [
+      {
+        name: 'projectName',
+        message: 'Your project name',
+        default: true
+      }
+    ];
+
+    return this.prompt(prompts).then((props) => {
+      this.props = props;
+    });
+  }
+
+  default() {
+    this.composeWith(require.resolve('generator-license'), {
+      licensePrompt: 'Which license do you want to use?',
+      defaultLicense: 'MIT'
+    });
+
+    if (path.basename(this.destinationPath()) !== this.props.projectName) {
+      this.log(
+        `Your generator must be inside a folder named ${this.props.projectName}
+        I'll automatically create this folder.`
+      );
+      mkdirp(this.props.projectName);
+      this.destinationRoot(this.destinationPath(this.props.projectName));
+    }
+  }
+
+  writing() {
+    const templates = fg.sync([this.templatePath('**/*')]);
+    const templatesRoot = this.templatePath();
+    templates.forEach((templatePath) => {
+      const templateRelativePath = templatePath.replace(templatesRoot + path.sep, '');
+      const paths = templateRelativePath.split(path.sep);
+      const templateName = paths.pop();
+      if (!templateName.startsWith('_')) {
+        return true;
+      }
+      if (paths.length > 0) {
+        mkdirp(this.destinationPath(paths.join(path.sep)));
+      }
+      this.fs.copyTpl(
+        templatePath,
+        this.destinationPath([...paths, templateName.slice(1)].join(path.sep)),
+        this.props
+      );
+      return true;
+    });
+  }
+};
